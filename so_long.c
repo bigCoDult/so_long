95e63cd4481f353f5383675f7c3adfc70396ed25
bigCoDult <qqqqmmss11@gmail.com>
2024-07-02 18:53:03 +0900

    feat : made libmlx.a

--- so_long.c
+++ so_long.c
@@ -1,6 +1,18 @@
 #include "so_long.h"
 
-int so_long(int fd)
+void so_long(int fd)
 {
-	
+	void *mlx_ptr;
+	int size_x = 500;
+	int size_y = 500;
+	char *title = "hi";
+	mlx_init();
+	mlx_new_window(mlx_ptr, size_x, size_y, title);
+
+}
+
+int	main(void)
+{
+	so_long(0);
+	return (0);
 }
