bb02c765224657ce4cd845956bf4954045029055
bigCoDult <qqqqmmss11@gmail.com>
2024-08-12 19:19:43 +0900

    chore : edit backtracking

--- validate.c
+++ validate.c
@@ -97,8 +97,6 @@ void	*set_map_str(int fd, t_total_data *total_data)
 	return (NULL);
 }
 
-
-
 bool	is_proper_chars(t_map_data *map_data)
 {
 	int index;
@@ -127,5 +125,71 @@ bool	is_proper_chars(t_map_data *map_data)
 	if (count_person != 1 || count_exit != 1 || count_collect < 1)
 		return (false);
 	return (true);
+}
+
+#include "so_long.h"
+
+bool	backtracking(t_char_map *char_map)
+{
+	t_cordi	*person;
+	person = get_cordi(char_map, PERSON);
+	return (is_possible(char_map, person));
+}
+
+t_cordi	*get_cordi(t_char_map *char_map, char c)
+{
+	t_cordi	*cordi;
+
+	cordi->row = 0;
+	while (cordi->row < char_map->row_size)
+	{
+		cordi->col = 0;
+		while (cordi->col < char_map->col_size)
+		{
+			if (char_map->map_cordi[cordi->row][cordi->col] == c)
+				return (cordi);
+			cordi->col++;
+		}
+		cordi->row++;
+	}
+	return (NULL);
+}
+
+bool	is_possible(t_char_map *char_map, t_cordi *person)
+{
+	if (char_map->map_cordi[person->row + 1][person->col] == EXIT)
+		return (true);
+	else if (char_map->map_cordi[person->row - 1][person->col] == EXIT)
+		return (true);
+	else if (char_map->map_cordi[person->row][person->col + 1] == EXIT)
+		return (true);
+	else if (char_map->map_cordi[person->row][person->col - 1] == EXIT)
+		return (true);
+	else if (char_map->map_cordi[person->row + 1][person->col] == EMPTY)
+	{
+		char_map->map_cordi[person->row][person->col] == WALL;
+		person->row += 1;
+		return (is_possible(char_map, person));
+	}
+	else if (char_map->map_cordi[person->row - 1][person->col] == EMPTY)
+	{
+		char_map->map_cordi[person->row][person->col] == WALL;
+		person->row -= 1;
+		return (is_possible(char_map, person));
+	}
+	else if (char_map->map_cordi[person->row][person->col + 1] == EMPTY)
+	{
+		char_map->map_cordi[person->row][person->col] == WALL;
+		person->col += 1;
+		return (is_possible(char_map, person));
+	}
+	else if (char_map->map_cordi[person->row][person->col - 1] == EMPTY)
+	{
+		char_map->map_cordi[person->row][person->col] == WALL;
+		person->col += 1;
+		return (is_possible(char_map, person));
+	}
+	else
+		return (false);
 }
 
