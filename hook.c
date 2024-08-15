03742ff5eb1c6ce83ba5c778e4b9871532d3a9fb
bigCoDult <qqqqmmss11@gmail.com>
2024-08-15 18:06:33 +0900

    chore : editing

--- hook.c
+++ hook.c
@@ -29,84 +29,80 @@ int move_person(int key, void *param)
 	}
 	else if (key == KEY_W || key == KEY_UP)
 	{
-			// printf("move up\n");
-			if (total_data->map_data->char_map[person->row - 1][person->col] == WALL)
-				return (0);
-			if (total_data->map_data->char_map[person->row - 1][person->col] == COLLECT)
-				total_data->map_data->count_collect--;
-			if (total_data->map_data->char_map[person->row - 1][person->col] == EXIT && total_data->map_data->count_collect != 0)
-				return (0);
-			if (total_data->map_data->char_map[person->row - 1][person->col] == EXIT && total_data->map_data->count_collect == 0)
-			{
-					end_game(total_data, person);
-				return (0);
-			}
-			total_data->map_data->char_map[person->row][person->col] = '0';
-			total_data->map_data->char_map[person->row - 1][person->col] = 'P';
-			// printf("count_collect : %d\n", total_data->map_data->count_collect);
-			// printf("you step : %d", ++step);
-			draw_map(total_data);
+		if (total_data->map_data->char_map[person->row - 1][person->col] == WALL)
+			return (0);
+		if (total_data->map_data->char_map[person->row - 1][person->col] == COLLECT)
+			total_data->map_data->count_collect--;
+		if (total_data->map_data->char_map[person->row - 1][person->col] == EXIT && total_data->map_data->count_collect != 0)
+			return (0);
+		if (total_data->map_data->char_map[person->row - 1][person->col] == EXIT && total_data->map_data->count_collect == 0)
+		{
+				end_game(total_data, person);
+			return (0);
+		}
+		total_data->map_data->char_map[person->row][person->col] = '0';
+		total_data->map_data->char_map[person->row - 1][person->col] = 'P';
+		// printf("count_collect : %d\n", total_data->map_data->count_collect);
+		// printf("you step : %d", ++step);
+		draw_map(total_data);
 	}
 	else if (key == KEY_S || key == KEY_DOWN)
 	{
-			// printf("move down\n");
-			if (total_data->map_data->char_map[person->row + 1][person->col] == '1')
-				return (0);
-			if (total_data->map_data->char_map[person->row + 1][person->col] == COLLECT)
-				total_data->map_data->count_collect--;
-			if (total_data->map_data->char_map[person->row + 1][person->col] == EXIT && total_data->map_data->count_collect != 0)
-				return (0);
-			if (total_data->map_data->char_map[person->row + 1][person->col] == EXIT && total_data->map_data->count_collect == 0)
-			{
-				end_game(total_data, person);
-				return (0);
-			}
-			total_data->map_data->char_map[person->row][person->col] = '0';
-			total_data->map_data->char_map[person->row + 1][person->col] = 'P';
-			// printf("count_collect : %d\n", total_data->map_data->count_collect);
-			// printf("you step : %d", ++step);
-			draw_map(total_data);
+		if (total_data->map_data->char_map[person->row + 1][person->col] == '1')
+			return (0);
+		if (total_data->map_data->char_map[person->row + 1][person->col] == COLLECT)
+			total_data->map_data->count_collect--;
+		if (total_data->map_data->char_map[person->row + 1][person->col] == EXIT && total_data->map_data->count_collect != 0)
+			return (0);
+		if (total_data->map_data->char_map[person->row + 1][person->col] == EXIT && total_data->map_data->count_collect == 0)
+		{
+			end_game(total_data, person);
+			return (0);
+		}
+		total_data->map_data->char_map[person->row][person->col] = '0';
+		total_data->map_data->char_map[person->row + 1][person->col] = 'P';
+		// printf("count_collect : %d\n", total_data->map_data->count_collect);
+		// printf("you step : %d", ++step);
+		draw_map(total_data);
 	}
 	else if (key == KEY_A || key == KEY_LEFT)
 	{
-			// printf("move left\n");
-			if (total_data->map_data->char_map[person->row][person->col - 1] == '1')
-				return (0);
-			if (total_data->map_data->char_map[person->row][person->col - 1] == COLLECT)
-				total_data->map_data->count_collect--;
-			if (total_data->map_data->char_map[person->row][person->col - 1] == EXIT && total_data->map_data->count_collect != 0)
-				return (0);
-			if (total_data->map_data->char_map[person->row][person->col - 1] == EXIT && total_data->map_data->count_collect == 0)
-			{
-				end_game(total_data, person);
-				return (0);
-			}
-			total_data->map_data->char_map[person->row][person->col] = '0';
-			total_data->map_data->char_map[person->row][person->col - 1] = 'P';
-			// printf("count_collect : %d\n", total_data->map_data->count_collect);
-			// printf("you step : %d", ++step);
-			draw_map(total_data);
+		if (total_data->map_data->char_map[person->row][person->col - 1] == '1')
+			return (0);
+		if (total_data->map_data->char_map[person->row][person->col - 1] == COLLECT)
+			total_data->map_data->count_collect--;
+		if (total_data->map_data->char_map[person->row][person->col - 1] == EXIT && total_data->map_data->count_collect != 0)
+			return (0);
+		if (total_data->map_data->char_map[person->row][person->col - 1] == EXIT && total_data->map_data->count_collect == 0)
+		{
+			end_game(total_data, person);
+			return (0);
+		}
+		total_data->map_data->char_map[person->row][person->col] = '0';
+		total_data->map_data->char_map[person->row][person->col - 1] = 'P';
+		// printf("count_collect : %d\n", total_data->map_data->count_collect);
+		// printf("you step : %d", ++step);
+		draw_map(total_data);
 	}
 	else if (key == KEY_D || key == KEY_RIGHT)
 	{
-			// printf("move right\n");
-			if (total_data->map_data->char_map[person->row][person->col + 1] == '1')
-				return (0);
-			if (total_data->map_data->char_map[person->row][person->col + 1] == COLLECT)
-				total_data->map_data->count_collect--;
-			if (total_data->map_data->char_map[person->row][person->col + 1] == EXIT && total_data->map_data->count_collect != 0)
-				return (0);
-			if (total_data->map_data->char_map[person->row][person->col + 1] == EXIT && total_data->map_data->count_collect == 0)
-			{
-				end_game(total_data, person);
-				return (0);
-			}
+		if (total_data->map_data->char_map[person->row][person->col + 1] == '1')
+			return (0);
+		if (total_data->map_data->char_map[person->row][person->col + 1] == COLLECT)
+			total_data->map_data->count_collect--;
+		if (total_data->map_data->char_map[person->row][person->col + 1] == EXIT && total_data->map_data->count_collect != 0)
+			return (0);
+		if (total_data->map_data->char_map[person->row][person->col + 1] == EXIT && total_data->map_data->count_collect == 0)
+		{
+			end_game(total_data, person);
+			return (0);
+		}
 
-			total_data->map_data->char_map[person->row][person->col] = '0';
-			total_data->map_data->char_map[person->row][person->col + 1] = 'P';
-			// printf("count_collect : %d\n", total_data->map_data->count_collect);
-			// printf("you step : %d", ++step);
-			draw_map(total_data);
+		total_data->map_data->char_map[person->row][person->col] = '0';
+		total_data->map_data->char_map[person->row][person->col + 1] = 'P';
+		// printf("count_collect : %d\n", total_data->map_data->count_collect);
+		// printf("you step : %d", ++step);
+		draw_map(total_data);
 	}
 	
 	return (0);
